80a41e65daac4136912e7ec69722b044a6ce0ce6
bfuller7141 <160172286+bfuller7141@users.noreply.github.com>
2025-07-08 20:08:47 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.moderndegreeservices.com',
-  integrations: [sitemap()],
   trailingSlash: 'never',
 });
